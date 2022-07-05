620b617b521adbedb8a7c0bcfd03b49ed2760d29
_Greenkiller_ <ericperonetti00@gmail.com>
2022-07-05 15:43:30 +0200

    [WIP] Camera

--- Assets/GameController.cs
+++ Assets/GameController.cs
@@ -6,13 +6,11 @@ public class GameController : MonoBehaviour
 {
     public GameObject playerList;
 
-    // Start is called before the first frame update
     void Start()
     {
         
     }
 
-    // Update is called once per frame
     void Update()
     {
         if (Input.GetKeyDown(Constants.KeyBindings.PlayerList))
