8616b4b977507465daab8b2fabb2ed0a4cec7213
_Greenkiller_ <ericperonetti00@gmail.com>
2022-07-08 01:55:20 +0200

    [WIP] Terrain generator
    Tests with cellular noise for biomes -> Not very natural...

--- Assets/Chunk.cs
+++ Assets/Chunk.cs
@@ -132,8 +132,18 @@ public static class ChunkUtils
 
     public static BiomeData GetBiome(float worldX, float worldZ, List<BiomeData> biomes, SeedManager seedManager)
     {
-        float biomeHeightMap = Mathf.PerlinNoise((seedManager.SeedX + worldX) / 20f, (seedManager.SeedZ + worldZ) / 20f);
-        biomeHeightMap = Mathf.Clamp01(biomeHeightMap);
+        var fn = new FastNoiseLite();
+        fn.SetSeed((seedManager.SeedX * seedManager.SeedZ).GetHashCode());
+        fn.SetNoiseType(FastNoiseLite.NoiseType.Cellular);
+        fn.SetFrequency(0.02f);
+        fn.SetCellularReturnType(FastNoiseLite.CellularReturnType.Distance2Div);
+        fn.SetCellularDistanceFunction(FastNoiseLite.CellularDistanceFunction.Euclidean);
+        fn.SetCellularDistanceFunction(FastNoiseLite.CellularDistanceFunction.Hybrid);
+        fn.SetCellularJitter(0.4f);
+
+        float biomeHeightMap = Mathf.Clamp01(-fn.GetNoise(worldX, worldZ));
+        /*float biomeHeightMap = Mathf.PerlinNoise((seedManager.SeedX + worldX) / 20f, (seedManager.SeedZ + worldZ) / 20f);
+        biomeHeightMap = Mathf.Clamp01(biomeHeightMap);*/
 
         float totalProbability = biomes.Sum(biome => biome.SpawnProbability);
 
@@ -155,6 +165,43 @@ public static class ChunkUtils
     public static float GetHeight(float worldX, float worldZ, List<BiomeData> biomes, SeedManager seedManager)
     {
         var biome = GetBiome(worldX, worldZ, biomes, seedManager);
+        /*var seededX = seedManager.SeedX + worldX;
+        var seededZ = seedManager.SeedZ + worldZ;
+
+        int scale = 100;
+        int octaves = 2;
+        float persistance = 0.46f;
+        float lacunarity = 1.5f;
+        float exponentiation = 2.5f;
+        float height = 200f;
+
+        float xs = seededX / scale;
+        float zs = seededZ / scale;
+        float G = Mathf.Pow(2f, -persistance);
+        float amplitude = 1f;
+        float frequency = 1f;
+        float normalization = 0f;
+        float total = 0f;
+
+        for (int o = 0; o < octaves; o++)
+        {
+            var noiseValue = Mathf.PerlinNoise(xs * frequency, zs * frequency) * 0.5f + 0.5f;
+            total += noiseValue * amplitude;
+            normalization += amplitude;
+            amplitude *= G;
+            frequency *= lacunarity;
+        }
+
+        var nv = Mathf.PerlinNoise(xs * biome.Frequency, zs * biome.Frequency);
+        total += nv * biome.Amplitude * G;
+        normalization += biome.Amplitude * G;
+
+        total /= normalization;
+
+        var y = Mathf.Pow(total, exponentiation) * height;
+
+        return y;*/
+
         return Mathf.PerlinNoise((seedManager.SeedX + worldX) * biome.Frequency, (seedManager.SeedZ + worldZ) * biome.Frequency) * biome.Amplitude;
     }
 
